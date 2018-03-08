f1d301f3421168342607157592286e5ae23b791b
Wanchalerm Yuphasuk <enjizen.r@gmail.com>
2018-03-08 12:33:42 +0700

    remove oncreate

--- app/src/main/java/cockatoo/enjizen/myapplicationtemplate/activity/BaseActivity.java
+++ app/src/main/java/cockatoo/enjizen/myapplicationtemplate/activity/BaseActivity.java
@@ -1,8 +1,6 @@
 package cockatoo.enjizen.myapplicationtemplate.activity;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -37,14 +35,7 @@ public class BaseActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
 
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-
-
-
-    }
 
     protected void setupDrawerLayout() {
        setSupportActionBar(toolbar);
